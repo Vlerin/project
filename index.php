78d79135479b04734f2911cbc5db507c2721426a
Vlerin <nodpie312@gmail.com>
2019-11-19 16:29:55 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Jeffrey Chang; period 1]</p>'; ?> 
+    
+      <li><a href="index.html">index</a></li></br>
+      <li><a href="Chang_AboutMe.html">About Me</a></li></br>
+      <li><a href="Chang_Last_Projects.html">Projects</a></li></br>
+      <li><a href="Chang_Resume.html">Resume</a></li></br>
+      <li><a href="Chang_ContactMe.html">Contact Me</a></li></br>
+
+  </body>
+</html>
